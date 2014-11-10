627f3a3b85bb2fce5e5849cf7066a49bf1b2baca
Michael Friedrich <michael.friedrich@netways.de>
2014-11-10 20:54:58 +0100

    Rebase against latest master, s/OpenTSDBWriter/OpenTsdbWriter/
    refs #7256

--- lib/perfdata/opentsdbwriter.cpp
+++ lib/perfdata/opentsdbwriter.cpp
@@ -26,7 +26,7 @@
 #include "base/tcpsocket.hpp"
 #include "base/dynamictype.hpp"
 #include "base/objectlock.hpp"
-#include "base/logger_fwd.hpp"
+#include "base/logger.hpp"
 #include "base/convert.hpp"
 #include "base/utility.hpp"
 #include "base/application.hpp"
@@ -42,15 +42,15 @@
 
 using namespace icinga;
 
-REGISTER_TYPE(OpenTSDBWriter);
+REGISTER_TYPE(OpenTsdbWriter);
 
-REGISTER_STATSFUNCTION(OpenTSDBWriterStats, &OpenTSDBWriter::StatsFunc);
+REGISTER_STATSFUNCTION(OpenTsdbWriterStats, &OpenTsdbWriter::StatsFunc);
 
-Value OpenTSDBWriter::StatsFunc(const Dictionary::Ptr& status, const Array::Ptr&)
+Value OpenTsdbWriter::StatsFunc(const Dictionary::Ptr& status, const Array::Ptr&)
 {
-	Dictionary::Ptr nodes = make_shared<Dictionary>();
+	Dictionary::Ptr nodes = new Dictionary();
 
-	BOOST_FOREACH(const OpenTSDBWriter::Ptr& opentsdbwriter, DynamicType::GetObjectsByType<OpenTSDBWriter>()) {
+	BOOST_FOREACH(const OpenTsdbWriter::Ptr& opentsdbwriter, DynamicType::GetObjectsByType<OpenTsdbWriter>()) {
 		nodes->Set(opentsdbwriter->GetName(), 1); //add more stats
 	}
 
@@ -59,39 +59,39 @@ Value OpenTSDBWriter::StatsFunc(const Dictionary::Ptr& status, const Array::Ptr&
 	return 0;
 }
 
-void OpenTSDBWriter::Start(void)
+void OpenTsdbWriter::Start(void)
 {
 	DynamicObject::Start();
 
-	m_ReconnectTimer = make_shared<Timer>();
+	m_ReconnectTimer = new Timer();
 	m_ReconnectTimer->SetInterval(10);
-	m_ReconnectTimer->OnTimerExpired.connect(boost::bind(&OpenTSDBWriter::ReconnectTimerHandler, this));
+	m_ReconnectTimer->OnTimerExpired.connect(boost::bind(&OpenTsdbWriter::ReconnectTimerHandler, this));
 	m_ReconnectTimer->Start();
 	m_ReconnectTimer->Reschedule(0);
 
-	Service::OnNewCheckResult.connect(boost::bind(&OpenTSDBWriter::CheckResultHandler, this, _1, _2));
+	Service::OnNewCheckResult.connect(boost::bind(&OpenTsdbWriter::CheckResultHandler, this, _1, _2));
 }
 
-void OpenTSDBWriter::ReconnectTimerHandler(void)
+void OpenTsdbWriter::ReconnectTimerHandler(void)
 {
 	if (m_Stream)
 		return;
 
-	TcpSocket::Ptr socket = make_shared<TcpSocket>();
+	TcpSocket::Ptr socket = new TcpSocket();
 
-	Log(LogNotice, "OpenTSDBWriter", "Reconnect to tcp socket on host '" + GetHost() + "' port '" + GetPort() + "'.");
+	Log(LogNotice, "OpenTsdbWriter", "Reconnect to tcp socket on host '" + GetHost() + "' port '" + GetPort() + "'.");
 
 	try {
 		socket->Connect(GetHost(), GetPort());
 	} catch (std::exception&) {
-		Log(LogCritical, "OpenTSDBWriter", "Can't connect to tcp socket on host '" + GetHost() + "' port '" + GetPort() + "'.");
+		Log(LogCritical, "OpenTsdbWriter", "Can't connect to tcp socket on host '" + GetHost() + "' port '" + GetPort() + "'.");
 		return;
 	}
 
-	m_Stream = make_shared<NetworkStream>(socket);
+	m_Stream = new NetworkStream(socket);
 }
 
-void OpenTSDBWriter::CheckResultHandler(const Checkable::Ptr& checkable, const CheckResult::Ptr& cr)
+void OpenTsdbWriter::CheckResultHandler(const Checkable::Ptr& checkable, const CheckResult::Ptr& cr)
 {
 	CONTEXT("Processing check result for '" + checkable->GetName() + "'");
 
@@ -143,7 +143,7 @@ void OpenTSDBWriter::CheckResultHandler(const Checkable::Ptr& checkable, const C
 	SendMetric(metric + ".execution_time", tags, Service::CalculateExecutionTime(cr));
 }
 
-void OpenTSDBWriter::SendPerfdata(const String& metric, const std::map<String, String>& tags, const CheckResult::Ptr& cr)
+void OpenTsdbWriter::SendPerfdata(const String& metric, const std::map<String, String>& tags, const CheckResult::Ptr& cr)
 {
 	Array::Ptr perfdata = cr->GetPerformanceData();
 
@@ -160,7 +160,7 @@ void OpenTSDBWriter::SendPerfdata(const String& metric, const std::map<String, S
 			try {
 				pdv = PerfdataValue::Parse(val);
 			} catch (const std::exception&) {
-				Log(LogWarning, "OpenTSDBWriter", "Ignoring invalid perfdata value: " + val);
+				Log(LogWarning, "OpenTsdbWriter", "Ignoring invalid perfdata value: " + val);
 				continue;
 			}
 		}
@@ -182,7 +182,7 @@ void OpenTSDBWriter::SendPerfdata(const String& metric, const std::map<String, S
 	}
 }
 
-void OpenTSDBWriter::SendMetric(const String& metric, const std::map<String, String>& tags, double value)
+void OpenTsdbWriter::SendMetric(const String& metric, const std::map<String, String>& tags, double value)
 {
     String tags_string = "";
     BOOST_FOREACH(const Dictionary::Pair& tag, tags) {
@@ -195,7 +195,7 @@ void OpenTSDBWriter::SendMetric(const String& metric, const std::map<String, Str
     // "tags" must include at least one tag, we use  "host=HOSTNAME"
 	msgbuf << "put " << metric << " " << static_cast<long>(Utility::GetTime()) << " " << Convert::ToString(value) << " " << tags_string;
 
-	Log(LogDebug, "OpenTSDBWriter", "Add to metric list:'" + msgbuf.str() + "'.");
+	Log(LogDebug, "OpenTsdbWriter", "Add to metric list:'" + msgbuf.str() + "'.");
 
 	// do not send \n to debug log
 	msgbuf << "\n";
@@ -209,7 +209,7 @@ void OpenTSDBWriter::SendMetric(const String& metric, const std::map<String, Str
 	try {
 		m_Stream->Write(put.CStr(), put.GetLength());
 	} catch (const std::exception& ex) {
-		Log(LogCritical, "OpenTSDBWriter", "Cannot write to TCP socket on host '" + GetHost() + "' port '" + GetPort() + "'.");
+		Log(LogCritical, "OpenTsdbWriter", "Cannot write to TCP socket on host '" + GetHost() + "' port '" + GetPort() + "'.");
 
 		m_Stream.reset();
 	}
@@ -217,7 +217,7 @@ void OpenTSDBWriter::SendMetric(const String& metric, const std::map<String, Str
 
 // for metric and tag name rules, see
 // http://opentsdb.net/docs/build/html/user_guide/writing.html#metrics-and-tags
-String OpenTSDBWriter::EscapeTag(const String& str)
+String OpenTsdbWriter::EscapeTag(const String& str)
 {
 	String result = str;
 
@@ -227,7 +227,7 @@ String OpenTSDBWriter::EscapeTag(const String& str)
 	return result;
 }
 
-String OpenTSDBWriter::EscapeMetric(const String& str)
+String OpenTsdbWriter::EscapeMetric(const String& str)
 {
 	String result = str;
 
